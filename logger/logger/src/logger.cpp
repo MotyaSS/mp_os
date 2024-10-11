db77b89c2897836a7c54afa26afbfafa245822cc
motya <motayzz32@gmail.com>
2024-10-11 11:31:00 +0300

    labirovan

--- logger/logger/src/logger.cpp
+++ logger/logger/src/logger.cpp
@@ -1,45 +1,44 @@
 #include "../include/logger.h"
 #include <iomanip>
-#include <sstream>
 
 logger const *logger::trace(
-        std::string const &message) const noexcept
+    std::string const &message) const noexcept
 {
     return log(message, logger::severity::trace);
 }
 
 logger const *logger::debug(
-        std::string const &message) const noexcept
+    std::string const &message) const noexcept
 {
     return log(message, logger::severity::debug);
 }
 
 logger const *logger::information(
-        std::string const &message) const noexcept
+    std::string const &message) const noexcept
 {
     return log(message, logger::severity::information);
 }
 
 logger const *logger::warning(
-        std::string const &message) const noexcept
+    std::string const &message) const noexcept
 {
     return log(message, logger::severity::warning);
 }
 
 logger const *logger::error(
-        std::string const &message) const noexcept
+    std::string const &message) const noexcept
 {
     return log(message, logger::severity::error);
 }
 
 logger const *logger::critical(
-        std::string const &message) const noexcept
+    std::string const &message) const noexcept
 {
     return log(message, logger::severity::critical);
 }
 
 std::string logger::severity_to_string(
-        logger::severity severity)
+    logger::severity severity)
 {
     switch (severity)
     {
@@ -60,22 +59,12 @@ std::string logger::severity_to_string(
     throw std::out_of_range("Invalid severity value");
 }
 
-std::string logger::current_date_to_string() noexcept
+std::string logger::current_datetime_to_string() noexcept
 {
     auto time = std::time(nullptr);
 
     std::ostringstream result_stream;
-    result_stream << std::put_time(std::localtime(&time), "%d.%m.%Y");
-
-    return result_stream.str();
-}
-
-std::string logger::current_time_to_string() noexcept
-{
-    auto time = std::time(nullptr);
-
-    std::ostringstream result_stream;
-    result_stream << std::put_time(std::localtime(&time), "%H:%M:%S");
+    result_stream << std::put_time(std::localtime(&time), "%d.%m.%Y %H:%M:%S");
 
     return result_stream.str();
 }
